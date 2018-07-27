2797cdd532fe334f749d73740dd1eefba242d0ba
YaojunXiang <xiangyaojun@foxmail.com>
2018-07-27 23:29:05 +0800

    --update

--- 二叉树/二叉树遍历（非递归）.py
+++ 二叉树/二叉树遍历（非递归）.py
@@ -2,3 +2,33 @@
 二叉树三种不同顺序遍历的非递归算法
 
 '''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preOrder(self, root):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+
+    def inOrder(self, root):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+
+    def postOrder(self, root):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: List[TreeNode.val]
+        """
