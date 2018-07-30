88288af197b0d6ec0e8b3d6ff1bc53578370739b
YaojunXiang <xiangyaojun@foxmail.com>
2018-07-30 17:31:21 +0800

    --update

---
+++ 二叉树/重建二叉树.py
@@ -0,0 +1,43 @@
+# coding:utf-8
+
+'''
+根据中序遍历结果和前序遍历的结果重建二叉树
+'''
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def buildTree(self,pre,tin):
+        hashmap_inorder = {}
+        for i in range(len(tin)):
+            hashmap_inorder[tin[i]] = i
+        return self.helper(pre, 0, tin, 0, len(tin) - 1, hashmap_inorder)
+
+    def helper(self,pre_nums, pre_start, in_nums,in_start,in_end,hashmap_inorder):
+        # 前序遍历数组，前序开始index，中序遍历数组，中序开始index，中序结束index，中序数组的hashmap
+        if pre_start>len(pre_nums)-1 or in_start > in_end:
+            return
+        root = TreeNode(pre_nums[pre_start])
+        in_index = hashmap_inorder[pre_nums[pre_start]]
+        root.left = self.helper(pre_nums, pre_start+1,in_nums,in_start,in_index-1,hashmap_inorder)
+        root.right = self.helper(pre_nums, pre_start+in_index-in_start+1,in_nums,in_index+1,in_end,hashmap_inorder)
+        return root
+
+    def preOrder_Recursive(self, root):
+        if root!=None:
+            print(root.val)  # visit the current node
+            self.preOrder_Recursive(root.left)
+            self.preOrder_Recursive(root.right)
+            
+
+pre_order = [1,2,3,4,5,6,7]
+in_order = [3,2,4,1,6,5,7]
+solu = Solution()
+root = solu.buildTree(pre_order,in_order)
+solu.preOrder_Recursive(root)
--- 数组/得到数组第k大小的元素.py
+++ 数组/得到数组第k大小的元素.py
@@ -1,6 +1,37 @@
 # coding:utf-8
 
 '''
-1.任意无序数组中得到第k大小的元素
-2.旋转数组中得到得到第k大小的元素
+1.旋转数组中得到最小的元素
+解析：
+    
+2.旋转数组中得到目标元素
+解析：
+暴力解法：遍历一遍数组就行，算法时间复杂度O(n)
+二分查找：直接从数组中间位置开始寻找，永远在有序的一边判断是否存在目标元素，算法时间复杂度O(logn)
+
+3.任意无序数组中得到第k小的元素
+
 '''
+
+class Solution:
+    def minNumberInRotateArray(self,rotateArray):
+        #得到旋转数组的最小元素，存在两个有序数组
+        left = 0
+        right = len(rotateArray)-1
+        if rotateArray[left]<rotateArray[right]:
+            return rotateArray[0]
+        while rotateArray[left]>=rotateArray[right]:
+            if right == left + 1:
+                return rotateArray[right]
+            mid = (left+right)//2
+            if rotateArray[left] == rotateArray[mid]:
+               return min(rotateArray)
+            if rotateArray[left] < rotateArray[mid]:
+                #则mid元素位于第一个有序数组中,此时最小元素必然在右半边
+                left = mid
+            else:
+                right = mid
+        return rotateArray[right]
+
+solu = Solution()
+print(solu.minNumberInRotateArray([5,6,7,8,9,10,10,1,2,3,4]))
