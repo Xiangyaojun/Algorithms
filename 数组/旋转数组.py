8a4e786da705e7b5bac029d6a4f40bbdac3efbb0
YaojunXiang <xiangyaojun@foxmail.com>
2018-08-06 21:55:24 +0800

    --update

--- 数组/旋转数组.py
+++ 数组/旋转数组.py
@@ -22,10 +22,10 @@ leetcode 189
 class Solution:
     def rotate(self,nums,k):
         k = k%len(nums)
-        if k<=0 : return
-        self.reverse(nums,0, len(nums)-k-1)
-        self.reverse(nums,len(nums)-k, len(nums)-1)
-        self.reverse(nums,0,len(nums)-1)
+        if k <= 0: return
+        self.reverse(nums, 0, len(nums)-k-1)
+        self.reverse(nums, len(nums)-k, len(nums)-1)
+        self.reverse(nums, 0, len(nums)-1)
         
     def reverse(self, nums, begin, end):
         while begin < end:
--- 数组/荷兰国旗问题.py
+++ 数组/荷兰国旗问题.py
@@ -1,3 +1,5 @@
+# coding:utf-8
+
 '''
 一、题目
     现有红、白、蓝三个不同颜色的小球，乱序排序在一起，
@@ -7,29 +9,37 @@
     使得数组排列成“0000...00001111....11112222...2222”
 三、解题思路：
     * 借鉴快速排序中partition的过程
-    * 初始化： begin = 0，cur = 0，end = N-1
-    * 打算[0,begin)全是0, [begin,cur)全是1,
-      [cur,end)是未遍历的数, [end,size-1)全是2
+    * 初始化： left = 0，k = 0，right = N
+    * 打算[0,left]全是0, (left,k)全是1,
+      [k,right)是未遍历的区域, [right,len(nums))全是2
 四、解题步骤：
-    
+    划分三个区域
+    0：[0,left]
+    1:[left+1,right-1] 设置K移动
+    2:[right,len(nums)-1]
 '''
 class Solution:
     def sortColors(self,nums):
-        left=0
-        right=len(nums)
-        for k in range(0, right):#保证[left,right-1]区域为0
+        #未访问区域为[k,right)
+        left = -1
+        right = len(nums)
+        k = 0
+        while k > left and k < right:
             if nums[k] == 0:
-                temp = nums[left]
-                nums[left] = nums[k]
-                nums[k] = temp
-                left += 1
+                left = left + 1
+                self.swap(nums, left, k)
+            elif nums[k] == 1:
+                k += 1
             elif nums[k] == 2:
-                right -= 1
-                temp = nums[right]
-                nums[right] = nums[k]
-                nums[k] = temp
-                k -= 1
+                right = right - 1
+                self.swap(nums, right, k)
         return nums
+
+    def swap(self,nums, i, j):
+        temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+
 solu = Solution()
-nums = [2,1,2,2,0,0,1,2,0,2,1]
+nums = [2, 1, 2, 2, 0, 1, 2, 2, 1]
 print(solu.sortColors(nums))
